7c533a72a092c950b509e86e6bfcc8ac6de11803
caren <carenoliveirasilvapontes@gmail.com>
2023-02-17 17:51:26 -0300

    feat:comentarios

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -13,8 +13,8 @@ export default class PVE extends Battle {
 
   fight(): number {
     while (this._player1.lifePoints !== -1 
-      && this._monster.some((e) => e.lifePoints !== -1)) {
-      this._monster.forEach((e) => {
+      && this._monster.some((e) => e.lifePoints !== -1)) { // se a vida do player1 não for -1 e a vida de todos os monstros não for -1 ele continua rodando
+      this._monster.forEach((e) => { // foreach para ele lutar com todos os monstros 
         this._player1.attack(e);
         e.attack(this._player1);
       });
